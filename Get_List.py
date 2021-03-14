b919bc94bd137993475d95dfa08ec0615332e400
Leiuuuu <74398488+Leiuuuu@users.noreply.github.com>
2021-03-14 11:25:22 +0800

    Update Get_List.py

--- Get_List.py
+++ Get_List.py
@@ -9,7 +9,7 @@ import requests
 request_url = "https://aip.baidubce.com/rest/2.0/face/v3/faceset/group/getlist"
 
 params = "{\"start\":0,\"length\":100}"
-access_token = '24.fd9a12d5930bc3972e420d6c07fed82f.2592000.1616723726.282335-18630025'
+access_token = 'access_token'
 request_url = request_url + "?access_token=" + access_token
 headers = {'content-type': 'application/json'}
 response = requests.post(request_url, data=params, headers=headers)
