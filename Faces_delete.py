9490a53500ebe8fede78e675724a8748fad7fd2f
Leiuuuu <74398488+Leiuuuu@users.noreply.github.com>
2021-03-14 11:23:26 +0800

    Update Faces_delete.py

--- Faces_delete.py
+++ Faces_delete.py
@@ -14,7 +14,7 @@ import json
 request_url = "https://aip.baidubce.com/rest/2.0/face/v3/faceset/user/delete"
 
 params = "{\"user_id\":\"user1\",\"group_id\":\"group_repeat\"}"
-access_token = '24.fd9a12d5930bc3972e420d6c07fed82f.2592000.1616723726.282335-18630025'
+access_token = 'access_token'
 request_url = request_url + "?access_token=" + access_token
 headers = {'content-type': 'application/json'}
 response = requests.post(request_url, data=params, headers=headers)
